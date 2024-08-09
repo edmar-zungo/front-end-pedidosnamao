2cfea369c78dfeafa31ec8236490ff1221d12b26
edmar-zungo <edmarzungo0110@gmail.com>
2024-08-09 19:03:17 +0100

    rotas de acesso as entidades adicionadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { MesaComponent } from './components/entities/mesa/mesa.component';
+import { CardapioComponent } from './components/entities/cardapio/cardapio.component';
+import { ItemConsumoComponent } from './components/entities/item-consumo/item-consumo.component';
+import { ItemPedidoComponent } from './components/entities/item-pedido/item-pedido.component';
+import { PedidoComponent } from './components/entities/pedido/pedido.component';
 
 export const routes: Routes = [
-     {path: 'mesa', component: MesaComponent}
+     { path: 'mesa', component: MesaComponent },
+     { path: 'cardapio', component: CardapioComponent },
+     { path: 'item-consumo', component: ItemConsumoComponent },
+     { path: 'item-pedido', component: ItemPedidoComponent },
+     { path: 'pedido', component: PedidoComponent }
 ];
